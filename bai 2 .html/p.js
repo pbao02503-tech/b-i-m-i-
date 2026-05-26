95ec87a9f8777954ab8bbb77e83461177ec2a9f3
pbao02503-tech <pbao02503@gmail.com>
2026-05-26 13:31:11 +0700

    Delete bai 2 .html directory

--- bai 2 .html/p.js
+++
@@ -1,6 +0,0 @@
-function updateProductTotal(productItem) {
-    const quantity = parseInt($(productItem).find('.quantity-input').val());
-    const unitPrice = parseInt($(productItem).find('.unit-price').data('price'));
-    const total = quantity * unitPrice;
-    $(productItem).find('.total-price').text(formatMoney(total) + 'đ');
-}
--- bai 2 .html/p2.js
+++
@@ -1,9 +0,0 @@
-function updateCartSummary() {
-    let totalItems = 0;
-    let totalQuantity = 0;
-    let grandTotal = 0;
-
-    $('.product-item').each(function() {
-        // Tính tổng tất cả sản phẩm
-    });
-}
